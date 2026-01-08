c79f8b08cd4c364e47ea5e08db4b48635db8b939
luis <luisfernandosilva8855@gmail.com>
2026-01-08 16:10:23 -0300

    fix: basename condicional no react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 
 function App() {
+  const basename = import.meta.env.PROD ? '/PowerManBarbearia' : '/'
+
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Layout><Home /></Layout>} />
-        </Routes>
-      </div>
+    <Router basename={basename}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+      </Routes>
     </Router>
   )
 }
